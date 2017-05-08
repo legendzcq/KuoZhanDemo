378dce9cc87b1f2eb68f313135e422f138510df2
legend <499458212@qq.com>
2017-05-08 22:21:51 +0800

    预处理

--- SHowView.js
+++ SHowView.js
@@ -34,7 +34,8 @@ export default class SHowView extends Component {
              tempStyle = ShowViewStyleBig;
          }
 
-
+http://blog.csdn.net/u013224660/article/details/51163067     propTypes = {   http://www.jianshu.com/p/d4b7f8f322b6
+    http://guotaiyaxing.cn.segmentfault.com/q/1010000009182684
 
 
         return (
